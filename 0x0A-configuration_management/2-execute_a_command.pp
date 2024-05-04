94a8ac44266d488335e52ce94ef8d9938805c130
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-04 12:33:05 +0100

    execute command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named killmenow
 
-exec {'pkill killnow',
-path => '/usr/bin:/usr/sbin:/bin'
+exec {'pkill killnow':
+path => '/usr/bin:/usr/sbin:/bin',
 }
 
