7cc07cd06de6d3924f2b9bfd006038ba2e046dd8
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-05 12:33:29 +0000

    changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,16 @@
 # using puppet to connect password
 
 file { '/etc/ssh/ssh_config':
-  ensure => present,
+ ensure => present,
+}
+
+file_line { 'Turn off password auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication',
 }
 
 file_line { 'Declare Identity file':
- ensure => present,
  path   => '/etc/ssh/ssh_config',
  line   => 'IdentityFile ~/.ssh/school',
  match  => '^IdentityFile',
