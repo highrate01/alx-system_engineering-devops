1bed3165f814e923b36607ffa0d5deca09c93be6
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-04 06:38:55 +0000

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,32 +7,30 @@ import re
 import requests
 import sys
 
-
 if __name__ == '__main__':
     base_url = "https://jsonplaceholder.typicode.com"
+
     if len(sys.argv) > 1:
-
         if re.fullmatch(r'\d+', sys.argv[1]):
-
             employee_id = int(sys.argv[1])
+            user_response = requests.get(f"{base_url}/users/{employee_id}")
+            user = user_response.json()
+            emp_name = user.get('name')
 
-            response = requests.get(f"{base_url}/users/{employee_id}")
-            user = response.json()
+            todos_response = requests.get(f"{base_url}/todos")
+            all_tasks = todos_response.json()
+            user_tasks = list(filter(lambda x: x.get('userId') == employee_id,
+                              all_tasks))
+            completed_tasks = list(filter(lambda x: x.get('completed'),
+                                          user_tasks))
 
-            params = {"userId": employee_id}
-
-            todos_response = requests.get(base_url + "/todos", params=params)
-
-            todos = todos_response.json()
-
-            completed = []
-
-            for todo in todos:
-                if todo.get("completed") is True:
-                    completed.append(todo.get("title"))
-
-            print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                len(completed), len(todos)))
-
-            for complete in completed:
-                print("\t{}".format(complete))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(user_tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t{}'.format(task.get('title')))
