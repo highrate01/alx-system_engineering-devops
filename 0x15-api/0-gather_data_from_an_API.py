8b310dab5ded2547e5bd8485a535b2955ed41508
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-04 06:23:34 +0000

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                 if todo.get("completed") is True:
                     completed.append(todo.get("title"))
 
-            print("Employee {} is done with tasks({}/{})".format(user.get("name"),
+            print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
                 len(completed), len(todos)))
 
             for complete in completed:
