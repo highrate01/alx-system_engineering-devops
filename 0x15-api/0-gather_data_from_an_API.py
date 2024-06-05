38c5981bacb171cdf462882675b749d3b5798157
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-05 20:30:12 +0000

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,46 +3,29 @@
 script that using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
-import re
-import requests
+from requests import get
 import sys
 
-if __name__ == '__main__':
-    base_url = "https://jsonplaceholder.typicode.com"
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            employee_id = int(sys.argv[1])
-            user_response = requests.get(f"{base_url}/users/{employee_id}")
-            user = user_response.json()
-            emp_name = user.get('name')
-            print("Employee Name: OK")
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-            todos_response = requests.get(f"{base_url}/todos")
-            all_tasks = todos_response.json()
-            user_tasks = list(filter(lambda x: x.get('userId') == employee_id,
-                              all_tasks))
-            completed_tasks = list(filter(lambda x: x.get('completed') is True,
-                                   user_tasks))
-            print("To Do Count: OK")
+    for i in data2:
+        if i.get('id') == int(sys.argv[1]):
+            employee = i.get('name')
 
-            print("First line formatting: OK")
-            print("Employee {} is done with tasks({}/{}):".format(
-                emp_name,
-                len(completed_tasks),
-                len(user_tasks)
-            ))
+    for i in data:
+        if i.get('userId') == int(sys.argv[1]):
+            total += 1
 
-            for i, task in enumerate(completed_tasks, 1):
-                print(f"Task {i} in output: OK")
-            for i in range(len(completed_tasks) + 1, 13):
-                print(f"Task {i} not in output")
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
 
-            for i, task in enumerate(completed_tasks, 1):
-                print(f"Task {i} Formatting: OK")
-                print("\t{}".format(task.get('title')))
-            for i in range(len(completed_tasks) + 1, 12):
-                print(f"Task {i} Formatting: OK")
-        else:
-            print("Please provide a valid numeric employee ID.")
-    else:
-        print("Please provide an employee ID.")
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
